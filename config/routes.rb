8d3b2eb48b3ca01054d76d248f64730d59aa0437
cesarlopezfa <cesarlopezfa@gmail.com>
2020-08-09 09:29:35 +0200

    Remove SayController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'store/index', as: 'store/index'
   resources :products
-  get 'say/hello'
-  get 'say/goodbye'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
