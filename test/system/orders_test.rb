1ca38f8db97c1ef04f6fa88ff7261213380ed5b2
cesarlopezfa <cesarlopezfa@gmail.com>
2020-08-13 23:10:02 +0200

    Verify mail is send when creating an order in system test

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   setup do
     @order = orders(:one)
   end
@@ -19,6 +21,9 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test "check routing number" do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     click_on "Add to Cart", match: :first
@@ -27,12 +32,34 @@ class OrdersTest < ApplicationSystemTestCase
 
     fill_in "order_name", with: "Dave Thomas"
     fill_in "order_address", with: "123 Main Street"
-    fill_in "order_email", with: "dave@hey.com"
 
     assert_no_selector "#order_routing_number"
 
     select "Check", from: "Pay type"
 
     assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.delete_all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal "Sam Ruby <depot@example.com>", mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 end
