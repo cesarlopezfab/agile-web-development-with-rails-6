b83950638dc63a0ad4f160ac8813d027f0053f02
cesarlopezfa <cesarlopezfa@gmail.com>
2020-08-13 21:51:36 +0200

    Add system test for Order

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -5,4 +5,34 @@ class OrdersTest < ApplicationSystemTestCase
     @order = orders(:one)
   end
 
+  test "visiting the index" do
+    visit orders_url
+    assert_selector "h1", text: "Orders"
+  end
+
+  test "destroying a Order" do
+    visit orders_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+    assert_text "Order was successfully destroyed"
+  end
+
+  test "check routing number" do
+    visit store_index_url
+
+    click_on "Add to Cart", match: :first
+
+    click_on "Checkout"
+
+    fill_in "order_name", with: "Dave Thomas"
+    fill_in "order_address", with: "123 Main Street"
+    fill_in "order_email", with: "dave@hey.com"
+
+    assert_no_selector "#order_routing_number"
+
+    select "Check", from: "Pay type"
+
+    assert_selector "#order_routing_number"
+  end
 end
